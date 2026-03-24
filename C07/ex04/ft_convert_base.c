41b3f2983349c3c9d6eade82ac5e43cd944ca6e6
Lucas Paiva <lpaiva234@gmail.com>
2026-03-24 00:13:03 -0300

    Create initial struture

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+char    *ft_convert_base(char *nbr, char *base_from, char *base_to);
+int     is_valid_base(char *base);
+char    *base_to_dec(char *num, char *base_from);
+
+char    *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+    if (!is_valid_base(base_from) || !is_valid_base(base_to))
+        return (NULL);
+    //skip spaces
+    // process signal
+    while (nbr[i] in base_from)
+    {
+        /* code */
+    }
+    
+}
+
+int     is_valid_base(char *base)
+{
+
+}
+
+char    *base_to_dec(char *num, char *base_from)
+{
+
+}
+
+char *dec_to_base(char *num, char *base_to)
+{
+
+}
