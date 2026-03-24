a0d68b8661cf10c334b21c9ac2d77378eeb90b99
Lucas Paiva <lpaiva234@gmail.com>
2026-03-24 11:03:11 -0300

    Implement is_valid_base function

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
-char    *ft_convert_base(char *nbr, char *base_from, char *base_to);
 int     is_valid_base(char *base);
-char    *base_to_dec(char *num, char *base_from);
+int     ft_atoi_base(char *str, char *base);
+char    *ft_convert_base(char *nbr, char *base_from, char *base_to);
 
 char    *ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
@@ -17,12 +17,34 @@ char    *ft_convert_base(char *nbr, char *base_from, char *base_to)
     
 }
 
+// checks if base is valid and returns its length, or 0 if not valid
 int     is_valid_base(char *base)
 {
+    int i;
+    int j;
 
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == '+' || base[i] == '-')
+            return (0);
+        if (base[i] == ' ' || (base[i] >= 9 && base[i] <= 13))
+            return (0);
+        j = i + 1;
+        while (base[j])
+        {
+            if (base[j] == base[i])
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    if (i < 2)
+        return (0);
+    return (i);
 }
 
-char    *base_to_dec(char *num, char *base_from)
+int     ft_atoi_base(char *str, char *base)
 {
 
 }
