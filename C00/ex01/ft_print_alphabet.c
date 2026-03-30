0011b9b8b07d7edf451776e4d461bcacea22f2a5
Lucas Paiva <lpaiva234@gmail.com>
2026-03-30 19:52:17 -0300

    Add ft_print_alphabet function

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/30 19:46:31 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/03/30 19:51:32 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+// int main(void)
+// {
+// 	ft_print_alphabet();
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
