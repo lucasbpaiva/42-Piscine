c1d012b827de15b62cb0fdb40a7a0aeae90869b5
Lucas Paiva <lpaiva234@gmail.com>
2026-03-31 17:05:00 -0300

    Add ft_print_numbers function

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 16:58:31 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/03/31 17:03:26 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	digit;
+
+	digit = '0';
+	while (digit <= '9')
+	{
+		write(1, &digit, 1);
+		digit++;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_print_numbers();
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
