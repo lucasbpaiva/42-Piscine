655ce83377461e6aa8712911a057967600733a8a
Lucas Paiva <lpaiva234@gmail.com>
2026-03-31 16:56:29 -0300

    Add ft_print_reverse_alphabet function

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 16:36:46 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/03/31 16:55:06 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_print_reverse_alphabet();
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
