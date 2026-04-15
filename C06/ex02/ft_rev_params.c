4f39167269a650875aa613aedb353289b4cb8fee
Lucas Paiva <lpaiva234@gmail.com>
2026-04-15 17:08:05 -0300

    Add ft_rev_params program

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 17:03:12 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/15 17:05:13 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+		i--;
+	}
+	return (0);
+}
