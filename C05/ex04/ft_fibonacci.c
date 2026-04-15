345aefcc354171ba6901851524e54f135d4219f6
Lucas Paiva <lpaiva234@gmail.com>
2026-04-14 22:20:14 -0300

    Add ft_fibonacci function

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 22:11:27 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/14 22:19:28 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	index = 6;
+
+// 	printf("fib(%d) = %d\n", index, ft_fibonacci(index));
+// 	return (0);
+// }
