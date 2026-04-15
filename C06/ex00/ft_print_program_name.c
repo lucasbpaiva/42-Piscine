4c8d8a51e863534d3a199a716697abb27ccb2f8c
Lucas Paiva <lpaiva234@gmail.com>
2026-04-15 16:51:23 -0300

    Add ft_print_program_name

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 16:08:06 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/15 16:45:33 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	write(1, argv[0], ft_strlen(argv[0]));
+	write(1, "\n", 1);
+	return (0);
+}
