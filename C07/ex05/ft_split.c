befdd3810786b37b1111373dff59cd7aedaf3b3a
Lucas Paiva <lpaiva234@gmail.com>
2026-04-17 12:44:43 -0300

    Add ft_split function

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 22:09:05 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/16 22:39:00 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/17 12:44:00 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,56 @@ char	*ft_strndup(char *src, int n)
 char	**ft_split(char *str, char *charset)
 {
 	char	**result;
+	int		start;
+	int		i;
+	int		j;
 
-	result = malloc(sizeof(char *) * count_words(str, charset));
+	result = malloc(sizeof(char *) * (count_words(str, charset) + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] && ft_includes(charset, str[i]))
+			i++;
+		if (str[i])
+		{
+			start = i;
+			while (str[i] && !ft_includes(charset, str[i]))
+				i++;
+			result[j++] = ft_strndup(&str[start], i - start);
+		}
+	}
+	result[j] = NULL;
+	return (result);
 }
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	**result;
+// 	int		i;
+
+// 	result = ft_split("  Hello  world from 42  ", " ");
+// 	if (!result)
+// 		return (1);
+
+// 	printf("[");
+// 	i = 0;
+// 	while (result[i])
+// 	{
+// 		if (i > 0)
+// 			printf(", ");
+// 		printf("'%s'", result[i]);
+// 		i++;
+// 	}
+// 	printf("]\n");
+
+// 	i = 0;
+// 	while (result[i])
+// 		free(result[i++]); // Free the memory from ft_strndup
+// 	free(result); // Free the memory from ft_split
+
+// 	return (0);
+// }
