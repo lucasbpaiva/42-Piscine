485c6a62e4afa08c9d5476f3f51ce9ae5d267e93
Lucas Paiva <lpaiva234@gmail.com>
2026-04-18 00:38:06 -0300

    Add ft_stock_str.h

---
+++ C08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/18 00:31:04 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/18 00:35:33 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+#endif
