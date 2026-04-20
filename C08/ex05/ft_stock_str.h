0a716814d2b74a70940b8fff55ddb970b1c9c7d6
Lucas Paiva <lpaiva234@gmail.com>
2026-04-20 15:34:58 -0300

    Add stdlib and function protoypes to header

---
+++ C08/ex05/ft_stock_str.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/18 00:31:04 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/20 15:23:22 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+# include <stdlib.h>
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av);
+int					ft_strlen(char *str);
+
+#endif
