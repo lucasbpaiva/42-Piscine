4262b210d61758fe86c41acd09e2e870abaf9c79
Lucas Paiva <lpaiva234@gmail.com>
2026-04-20 15:33:44 -0300

    Add ft_strs_to_tab here for compiling

---
+++ C08/ex05/ft_strs_to_tab.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/18 00:30:28 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/20 15:22:29 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*cpy;
+	int		i;
+
+	if (!src)
+		return (0);
+	cpy = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		cpy[i] = src[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+
+t_stock_str	create_stock(char *str)
+{
+	t_stock_str	item;
+
+	item.size = ft_strlen(str);
+	item.str = str;
+	item.copy = ft_strdup(str);
+	return (item);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*result;
+
+	result = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		result[i] = create_stock(av[i]);
+		if (!result[i].copy)
+		{
+			while (--i >= 0)
+				free(result[i].copy);
+			free(result);
+			return (NULL);
+		}
+		i++;
+	}
+	result[i].str = 0;
+	return (result);
+}
