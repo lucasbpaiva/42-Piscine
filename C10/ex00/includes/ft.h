56f55bbddb59639dff6a90fae663c8e509a91026
Lucas Paiva <lpaiva234@gmail.com>
2026-04-22 17:05:34 -0300

    Add header file

---
+++ C10/ex00/includes/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 15:39:05 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/22 16:01:04 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+void	ft_putstr_fd(int fd, char *str);
+void	ft_display_file(char *filename);
+
+#endif
