e4bfcf9d453558143d8b21c183ae46fe9bdf9aa9
Lucas Paiva <lpaiva234@gmail.com>
2026-04-23 13:48:18 -0300

    Add ft_display_error function

--- C10/ex01/srcs/ft_display_error.c
+++ C10/ex01/srcs/ft_display_error.c
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_error.c                                         :+:      :+:    :+:   */
+/*   ft_display_error.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/22 22:42:17 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/22 22:42:18 by lbalderr         ###   ########.fr       */
+/*   Created: 2026/04/23 12:20:59 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/23 12:36:07 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libgen.h>
+#include <string.h>
+#include <errno.h>
+#include "ft.h"
+
+void	ft_display_error(char *prog_name, char *file_name)
+{
+	ft_putstr_fd(2, basename(prog_name));
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, file_name);
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, strerror(errno));
+	ft_putstr_fd(2, "\n");
+}
