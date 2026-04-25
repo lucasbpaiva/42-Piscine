c13f21d2d4fa86455d7054e65f200be7a6079791
Lucas Paiva <lpaiva234@gmail.com>
2026-04-24 23:58:11 -0300

    Add struct typedef

--- C10/ex02/includes/ft.h
+++ C10/ex02/includes/ft.h
@@ -6,17 +6,24 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 15:39:31 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/24 15:43:54 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/24 17:00:59 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
+typedef struct s_info
+{
+	char	*prog;
+	char	*file;
+}	t_info;
+
 int		ft_atoi(char *str);
 int		ft_strcmp(char *s1, char *s2);
-int		load_buf(int fd, int nb, char *buf);
+int		load_buf(int fd, int nb, char *buf, t_info *info);
 void	ft_putstr_fd(char *str, int fd);
-void	display_last_n_bytes(int fd, int nb);
+void	display_last_n_bytes(int fd, int nb, t_info *info);
+void	ft_display_error(char *prog_name, char *file_name);
 
 #endif
