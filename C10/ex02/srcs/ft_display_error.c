5d14e36ad3e32f3b4989b292f072c4bb3c9d1297
Lucas Paiva <lpaiva234@gmail.com>
2026-04-24 23:59:14 -0300

    update parameter order

--- C10/ex02/srcs/ft_display_error.c
+++ C10/ex02/srcs/ft_display_error.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 16:21:14 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/24 16:22:48 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/24 16:25:34 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 
 void	ft_display_error(char *prog_name, char *file_name)
 {
-	ft_putstr_fd(2, basename(prog_name));
-	ft_putstr_fd(2, ": ");
-	ft_putstr_fd(2, file_name);
-	ft_putstr_fd(2, ": ");
-	ft_putstr_fd(2, strerror(errno));
-	ft_putstr_fd(2, "\n");
+	ft_putstr_fd(basename(prog_name), 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(file_name, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd("\n", 2);
 }
