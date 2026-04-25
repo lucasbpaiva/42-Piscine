58f9a8cc3051dceb36ca4b2bfcb66be39994cbb1
Lucas Paiva <lpaiva234@gmail.com>
2026-04-25 00:03:41 -0300

    Send program and file name to other functions

--- C10/ex02/srcs/main.c
+++ C10/ex02/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 14:07:56 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/24 15:34:53 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/24 17:10:48 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 
 int	main(int argc, char *argv[])
 {
-	int	nb;
-	int	fd;
-	int	i;
+	int		nb;
+	int		fd;
+	int		i;
+	t_info	info;
 
+	info.prog = argv[0];
 	if (argc >= 3 && ft_strcmp(argv[1], "-c") == 0)
 	{
 		nb = ft_atoi(argv[2]);
@@ -28,17 +30,18 @@ int	main(int argc, char *argv[])
 			ft_putstr_fd(argv[0], 2);
 			ft_putstr_fd(": illegal offset -- ", 2);
 			ft_putstr_fd(argv[2], 2);
-			ft_putstr_fd(": Invalid argument", 2);
+			ft_putstr_fd(": Invalid argument\n", 2);
 			return (0);
 		}
 		if (argc == 3)
 		{
-			display_last_n_bytes(0, nb);
+			display_last_n_bytes(0, nb, &info);
 			return (0);
 		}
 		i = 3;
 		while (i < argc)
 		{
+			info.file = argv[i];
 			if (argc > 4)
 			{
 				ft_putstr_fd("==> ", 1);
@@ -50,7 +53,7 @@ int	main(int argc, char *argv[])
 				ft_display_error(argv[0], argv[i]);
 			else
 			{
-				display_last_n_bytes(fd, nb);
+				display_last_n_bytes(fd, nb, &info);
 				close(fd);
 			}
 			if (i != argc - 1)
