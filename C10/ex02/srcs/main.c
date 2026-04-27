5103a00d5d635fc059edff47c00c7d6d63700b0c
Lucas Paiva <lpaiva234@gmail.com>
2026-04-27 16:04:16 -0300

    Add missing argments error treatment

--- C10/ex02/srcs/main.c
+++ C10/ex02/srcs/main.c
@@ -6,21 +6,13 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 14:07:56 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/27 15:24:51 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/27 15:54:12 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
 #include <unistd.h>
 #include <fcntl.h>
-
-void	display_offset_error(char *argv[])
-{
-	ft_putstr_fd(argv[0], 2);
-	ft_putstr_fd(": illegal offset -- ", 2);
-	ft_putstr_fd(argv[2], 2);
-	ft_putstr_fd(": Invalid argument\n", 2);
-}
 
 void	display_file_header(char *filename, int is_first)
 {
@@ -81,6 +73,8 @@ int	main(int argc, char *argv[])
 			return (0);
 		}
 		process_files(argc, argv, nb, &info);
-	}	
+	}
+	if (argc < 3)
+		display_argreq_error(argv[0]);
 	return (0);
 }
