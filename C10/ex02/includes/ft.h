6c6e1ab305b88c3c0dcc6a4ccf65dd79675f268a
Lucas Paiva <lpaiva234@gmail.com>
2026-04-27 15:31:51 -0300

    Remove internal function from header

--- C10/ex02/includes/ft.h
+++ C10/ex02/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 15:39:31 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/24 17:00:59 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/27 15:18:28 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ typedef struct s_info
 
 int		ft_atoi(char *str);
 int		ft_strcmp(char *s1, char *s2);
-int		load_buf(int fd, int nb, char *buf, t_info *info);
 void	ft_putstr_fd(char *str, int fd);
 void	display_last_n_bytes(int fd, int nb, t_info *info);
 void	ft_display_error(char *prog_name, char *file_name);
