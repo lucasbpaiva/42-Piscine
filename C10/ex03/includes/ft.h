b8559a9dd7a136d2c3a7957fef5f7ec051f96a1e
Lucas Paiva <lpaiva234@gmail.com>
2026-04-28 16:48:07 -0300

    Add more function prototypes

--- C10/ex03/includes/ft.h
+++ C10/ex03/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 17:22:58 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/28 15:30:01 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/28 16:34:21 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ typedef struct s_stream
 	long	offset;
 }	t_stream;
 
+int		ft_strcmp(char *s1, char *s2);
+void	ft_hexdump(t_stream *s);
 void	print_offset(long offset);
 void	ft_putstr_fd(char *str, int fd);
+void	ft_display_option_error(char *prog_name);
 void	print_hex_content(unsigned char *buf, int n);
 void	print_ascii_content(unsigned char *buf, int n);
 void	ft_display_error(char *prog_name, char *file_name);
