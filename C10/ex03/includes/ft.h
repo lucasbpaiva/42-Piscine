d8070b1a487792a526e03eeec202fc2fb068ddd8
Lucas Paiva <lpaiva234@gmail.com>
2026-04-28 14:02:57 -0300

    Add new print functions to header

--- C10/ex03/includes/ft.h
+++ C10/ex03/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 17:22:58 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/27 17:33:43 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/28 11:46:55 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 
 void	print_offset(long offset);
 void	ft_putstr_fd(char *str, int fd);
+void	print_hex_content(unsigned char *buf, int n);
+void	print_ascii_content(unsigned char *buf, int n);
 
 #endif
