a4b68f74fa081fbd18f8f4fef4860288e162c54a
Lucas Paiva <lpaiva234@gmail.com>
2026-04-28 15:55:29 -0300

    Add ft_hexdump function

--- C10/ex03/srcs/ft_hexdump.c
+++ C10/ex03/srcs/ft_hexdump.c
@@ -6,46 +6,69 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 17:19:51 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/27 22:03:17 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/28 15:53:32 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
+#include <unistd.h>
+#include <fcntl.h>
 
-void	print_offset(long offset)
+#define SIZE 16
+
+void	close_current_file(t_stream *s)
 {
-	char	result[9];
-	int		i;
-
-	result[8] = '\0';
-	i = 7;
-	while (i >= 0)
-	{
-		result[i] = HEX[offset % 16];
-		offset = offset / 16;
-		i--;
-	}
-	ft_putstr_fd(result, 1);
-	ft_putstr_fd("  ", 1);
+	close(s->fd);
+	s->fd = -1;
+	s->curr_arg++;
 }
 
-void	print_hex_content(unsigned char *buf, int n)
+int	get_next_chunk(t_stream *s, unsigned char *buf)
 {
-	int	i;
+	int		bytes_read;
+	int		total_collected;
+	char	*curr_file;
 
-	i = 0;
-	while (i < 16)
+	total_collected = 0;
+	while (total_collected < SIZE && s->curr_arg < s->argc)
 	{
-		if (i < n)
+		if (s->fd == -1)
 		{
-			write(1, &HEX[buf[i] / 16], 1);
-			write(1, &HEX[buf[i] % 16], 1);
-			write(1, " ", 1);
+			curr_file = s->argv[s->curr_arg];
+			s->fd = open(curr_file, O_RDONLY);
+			if (s->fd == -1)
+			{
+				ft_display_error(s->argv[0], curr_file);
+				s->curr_arg++;
+				continue ;
+			}
 		}
+		bytes_read = read(s->fd, buf + total_collected, SIZE - total_collected);
+		if (bytes_read <= 0)
+			close_current_file(s);
 		else
-			ft_putstr_fd("   ", 1);
-		if (i == 7 || i == 15)
-			write(1, " ", 1);
-		i++;
+			total_collected += bytes_read;
+	}
+	return (total_collected);
+}
+
+void	ft_hexdump(t_stream *s)
+{
+	unsigned char	buf[SIZE];
+	int				n;
+
+	n = get_next_chunk(s, buf);
+	while (n > 0)
+	{
+		print_offset(s->offset);
+		print_hex_content(buf, n);
+		print_ascii_content(buf, n);
+		s->offset += n;
+		n = get_next_chunk(s, buf);
+	}
+	if (s->offset > 0)
+	{
+		print_offset(s->offset);
+		ft_putstr_fd("\n", 1);
 	}
 }
