49d42e1acce2fbd080f29f86920b805e21ef5f06
Lucas Paiva <lpaiva234@gmail.com>
2026-04-29 15:35:59 -0300

    Add print_hex_pairs function

--- C10/ex03/srcs/ft_print_utils.c
+++ C10/ex03/srcs/ft_print_utils.c
@@ -6,20 +6,25 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 11:28:26 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/28 11:44:59 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/29 15:27:40 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
 #include <unistd.h>
 
-void	print_offset(long offset)
+void	print_offset(long offset, int mode)
 {
 	char	result[9];
+	int		len;
 	int		i;
 
-	result[8] = '\0';
-	i = 7;
+	if (mode == 1)
+		len = 8;
+	else
+		len = 7;
+	result[len] = '\0';
+	i = len - 1;
 	while (i >= 0)
 	{
 		result[i] = HEX[offset % 16];
@@ -27,7 +32,9 @@ void	print_offset(long offset)
 		i--;
 	}
 	ft_putstr_fd(result, 1);
-	ft_putstr_fd("  ", 1);
+	write(1, " ", 1);
+	if (mode == 1)
+		write(1, " ", 1);
 }
 
 void	print_hex_content(unsigned char *buf, int n)
@@ -66,5 +73,35 @@ void	print_ascii_content(unsigned char *buf, int n)
 		i++;
 	}
 	write(1, "|", 1);
+	write(1, "\n", 1);
+}
+
+void	write_hex_byte(t_uc c)
+{
+	write(1, &HEX[c / 16], 1);
+	write(1, &HEX[c % 16], 1);
+}
+
+void	print_hex_pairs(unsigned char *buf, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (i < n)
+		{
+			if (i + 1 < n)
+				write_hex_byte(buf[i + 1]);
+			else
+				write(1, "00", 2);
+			write_hex_byte(buf[i]);
+		}
+		else
+			write(1, "    ", 4);
+		if (i < 14)
+			write(1, " ", 1);
+		i += 2;
+	}
 	write(1, "\n", 1);
 }
