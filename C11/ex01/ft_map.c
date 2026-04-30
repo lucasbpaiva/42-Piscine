4797dee1635f8cf37b4b66af408a834ff0fc7772
Lucas Paiva <lpaiva234@gmail.com>
2026-04-30 11:47:11 -0300

    Add ft_map function

---
+++ C11/ex01/ft_map.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 11:07:18 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/30 11:13:41 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	*result;
+	int	i;
+
+	result = malloc(sizeof(int) * length);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		result[i] = f(tab[i]);
+		i++;
+	}
+	return (result);
+}
