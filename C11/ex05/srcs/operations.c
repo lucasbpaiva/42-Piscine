9bf392ad724ab30bb92517514ac65ee1552be5c0
Lucas Paiva <lpaiva234@gmail.com>
2026-04-30 16:38:36 -0300

    Rename functions

--- C11/ex05/srcs/operations.c
+++ C11/ex05/srcs/operations.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 15:57:45 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/30 16:07:24 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/30 16:26:50 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@ int	add(int a, int b)
 	return (a + b);
 }
 
-int	subtract(int a, int b)
+int	sub(int a, int b)
 {
 	return (a - b);
 }
 
-int	multiply(int a, int b)
+int	mul(int a, int b)
 {
 	return (a * b);
 }
 
 // Division by 0 error needs to be handled by the caller
-int	divide(int a, int b)
+int	div(int a, int b)
 {
 	return (a / b);
 }
 
 // Modulo by 0 error needs to be handled by the caller
-int	modulo(int a, int b)
+int	mod(int a, int b)
 {
 	return (a % b);
 }
