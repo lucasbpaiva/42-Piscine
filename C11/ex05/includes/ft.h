fd4fefc67d09a58d20060931912132797bb97812
Lucas Paiva <lpaiva234@gmail.com>
2026-04-30 16:09:35 -0300

    Add atoi and operations prototypes

---
+++ C11/ex05/includes/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 15:46:04 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/04/30 16:08:13 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+
+# define FT_H
+
+int	add(int a, int b);
+int	subtract(int a, int b);
+int	multiply(int a, int b);
+int	divide(int a, int b);
+int	modulo(int a, int b);
+int	ft_atoi(char *str);
+
+#endif
