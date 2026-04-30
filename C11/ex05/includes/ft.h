4c7806bb4c0e6a6531dd29090396d2a51a47c0e8
Lucas Paiva <lpaiva234@gmail.com>
2026-04-30 16:39:17 -0300

    Update function names

--- C11/ex05/includes/ft.h
+++ C11/ex05/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 15:46:04 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/30 16:08:13 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/04/30 16:27:17 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 # define FT_H
 
 int	add(int a, int b);
-int	subtract(int a, int b);
-int	multiply(int a, int b);
-int	divide(int a, int b);
-int	modulo(int a, int b);
+int	sub(int a, int b);
+int	mul(int a, int b);
+int	div(int a, int b);
+int	mod(int a, int b);
 int	ft_atoi(char *str);
 
 #endif
