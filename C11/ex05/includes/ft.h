1d44535944d22f91b2b56fd19622b3658713da60
Lucas Paiva <lpaiva234@gmail.com>
2026-05-04 12:00:16 -0300

    Add do-op function

--- C11/ex05/includes/ft.h
+++ C11/ex05/includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 15:46:04 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/04 11:31:56 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/04 11:59:50 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		div(int a, int b);
 int		mod(int a, int b);
 int		ft_atoi(char *str);
 void	ft_putnbr(int nb);
+void	do_op(int a, int b, char *op);
 void	ft_putstr_fd(char *str, int fd);
 
 #endif
--- C11/ex05/srcs/do-op.c
+++ C11/ex05/srcs/do-op.c
@@ -6,11 +6,12 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 16:11:11 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/30 16:42:49 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/04 11:57:30 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
+#include <unistd.h>
 
 int	get_op_index(char c)
 {
@@ -28,7 +29,7 @@ int	get_op_index(char c)
 		return (-1);
 }
 
-void	do_op(int a, int b, char op)
+void	do_op(int a, int b, char *op)
 {
 	int	(*operations[5])(int, int);
 	int	index;
@@ -39,10 +40,18 @@ void	do_op(int a, int b, char op)
 	operations[2] = mul;
 	operations[3] = div;
 	operations[4] = mod;
-	index = get_op_index(op);
-	if (index != -1)
+	if (ft_strlen(op) != 1)
 	{
-		result = operations[index](a, b);
-		ft_putnbr(result);
+		write(1, "0", 1);
+		return ;
 	}
+	index = get_op_index(op[0]);
+	if (index == -1)
+		write(1, "0", 1);
+	if (index == 3 && b == 0)
+		ft_putstr_fd("Stop : division by zero\n", 1);
+	else if (index == 4 && b == 0)
+		ft_putstr_fd("Stop : modulo by zero\n", 1);
+	else
+		ft_putnbr(operations[index](a, b));
 }
