e1c779481282489310c8edbccc7f5ade108ee9aa
Lucas Paiva <lpaiva234@gmail.com>
2026-05-04 12:45:28 -0300

    Add main

--- C11/ex05/srcs/main.c
+++ C11/ex05/srcs/main.c
@@ -6,7 +6,21 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 16:11:08 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/04/30 16:11:10 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/04 12:07:12 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft.h"
+
+int	main(int argc, char *argv[])
+{
+	int	val_1;
+	int	val_2;
+
+	if (argc != 4)
+		return (0);
+	val_1 = ft_atoi(argv[1]);
+	val_2 = ft_atoi(argv[3]);
+	do_op(val_1, val_2, argv[1]);
+	return (0);
+}
