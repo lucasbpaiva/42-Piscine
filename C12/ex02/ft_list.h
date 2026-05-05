5f029548c3f4ace33046d8c7392cbbfa6efc5b6b
Lucas Paiva <lpaiva234@gmail.com>
2026-05-05 14:18:04 -0300

    Add ft_list.h and create_elem

---
+++ C12/ex02/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/04 17:21:04 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+
+# define FT_LIST_H
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+t_list	*ft_create_elem(void *data);
+
+#endif
