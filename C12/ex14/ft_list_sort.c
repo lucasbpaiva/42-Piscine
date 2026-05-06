f3037a21fcb4483051e8ea38a9e8075cb1a78f4c
Lucas Paiva <lpaiva234@gmail.com>
2026-05-06 17:48:48 -0300

    Add merge_sort function

--- C12/ex14/ft_list_sort.c
+++ C12/ex14/ft_list_sort.c
@@ -6,15 +6,27 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 17:21:36 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/06 17:39:13 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/06 17:47:36 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
+t_list	*merge_sort(t_list *head)
+{
+	t_list	*second;
+
+	if (!head || !head->next)
+		return (head);
+	second = split_list(head);
+	head = merge_sort(head);
+	second = merge_sort(second);
+	return (merge(head, second));
+}
+
 void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
 	if (!begin_list || !cmp)
 		return ;
-	begin_list = mergeSort(*begin_list);
+	begin_list = merge_sort(*begin_list);
 }
