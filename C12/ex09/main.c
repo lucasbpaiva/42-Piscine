e4b0de68c7f3021d1c67df09a8061b69c89522f4
Lucas Paiva <lpaiva234@gmail.com>
2026-05-06 12:25:34 -0300

    Add main for testing

---
+++ C12/ex09/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 12:05:42 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/06 12:23:27 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+	write(1, "\n", 1);
+}
+
+void	put_data(void *data)
+{
+	if (data)
+		ft_putstr((char *)data);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_list	*my_list;
+
+	if (argc < 2)
+		return (0);
+	my_list = ft_list_push_strs(argc - 1, &argv[1]);
+	ft_list_foreach(my_list, put_data);
+	return (0);
+}
