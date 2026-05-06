91099e3d3c7f1fb9ae2de0ce5ef47370ec9d87a8
Lucas Paiva <lpaiva234@gmail.com>
2026-05-06 12:05:25 -0300

    Add ft_list_push_strs

--- C12/ex09/ft_list.h
+++ C12/ex09/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/06 12:02:35 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/06 12:04:37 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ typedef struct s_list
 	void			*data;
 }	t_list;
 
-t_list	*ft_create_elem(void *data);
 int		ft_list_size(t_list *begin_list);
 void	ft_print_list(t_list *node);
 void	ft_list_reverse(t_list **begin_list);
 void	ft_list_push_back(t_list **begin_list, void *data);
 void	ft_list_push_front(t_list **begin_list, void *data);
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *));
+t_list	*ft_create_elem(void *data);
+t_list	*ft_list_push_strs(int size, char **strs);
 
 #endif
