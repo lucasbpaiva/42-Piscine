3da8f6982b904379df6d43caaaf65974523d5d50
Lucas Paiva <lpaiva234@gmail.com>
2026-05-06 14:33:10 -0300

    Add ft_list_find function

---
+++ C12/ex11/ft_list_find.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 14:24:02 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/06 14:29:13 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*list_ptr;
+
+	if (!cmp || !begin_list)
+		return (0);
+	list_ptr = begin_list;
+	while (list_ptr)
+	{
+		if ((*cmp)(list_ptr->data, data_ref) == 0)
+			return (list_ptr);
+		list_ptr = list_ptr->next;
+	}
+	return (0);
+}
