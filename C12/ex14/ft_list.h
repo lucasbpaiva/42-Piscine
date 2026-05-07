904fb28d87a374aeab5009923a471bded06f1907
Lucas Paiva <lpaiva234@gmail.com>
2026-05-06 22:39:56 -0300

    Add ft_list_sort and ft_strcmp to header

--- C12/ex14/ft_list.h
+++ C12/ex14/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/06 12:04:37 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/06 21:35:04 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ typedef struct s_list
 	void			*data;
 }	t_list;
 
+int		ft_strcmp(char *s1, char *s2);
 int		ft_list_size(t_list *begin_list);
 void	ft_print_list(t_list *node);
 void	ft_list_reverse(t_list **begin_list);
+void	ft_list_sort(t_list **begin_list, int (*cmp)());
 void	ft_list_push_back(t_list **begin_list, void *data);
 void	ft_list_push_front(t_list **begin_list, void *data);
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *));
