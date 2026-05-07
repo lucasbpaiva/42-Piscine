20aad2d9b9139539cd327c58d8a31ee7bee0d3ab
Lucas Paiva <lpaiva234@gmail.com>
2026-05-07 16:01:29 -0300

    Add ft_list_find to ft_list.h

--- C12/ex11/ft_list.h
+++ C12/ex11/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/06 12:04:37 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/07 15:37:34 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_list
 	void			*data;
 }	t_list;
 
+int		ft_strcmp(char *s1, char *s2);
 int		ft_list_size(t_list *begin_list);
 void	ft_print_list(t_list *node);
 void	ft_list_reverse(t_list **begin_list);
@@ -28,5 +29,6 @@ void	ft_list_push_front(t_list **begin_list, void *data);
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *));
 t_list	*ft_create_elem(void *data);
 t_list	*ft_list_push_strs(int size, char **strs);
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)());
 
 #endif
