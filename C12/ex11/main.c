7b0b1a68c25ca72a44e1c2ca00a5c908e6e41506
Lucas Paiva <lpaiva234@gmail.com>
2026-05-07 16:02:08 -0300

    Add main for testing

---
+++ C12/ex11/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/07 15:22:45 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/07 15:54:49 by lbalderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	t_list	*list;
+	t_list	*found;
+	char	*secret_word;
+
+	secret_word = "42";
+	if (argc < 2)
+		return (0);
+	list = ft_list_push_strs(argc - 1, &argv[1]);
+	found = ft_list_find(list, secret_word, ft_strcmp);
+	if (found)
+	{
+		printf("Found secret word in list, ");
+		printf("secret_word: %s\n", (char *)found->data);
+	}
+	else
+		printf("List does not contain the secret word\n");
+	return (0);
+}
