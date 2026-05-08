41728a61ecdfb0c01bca92fe364c244da7fc76f5
Lucas Paiva <lpaiva234@gmail.com>
2026-05-08 17:03:43 -0300

    Use casting on function pointer to enable compilation

--- C12/ex10/ft_list_foreach_if.c
+++ C12/ex10/ft_list_foreach_if.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:59:38 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/06 14:15:12 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/08 16:52:47 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_list_foreach_if(t_list *begin_list, void (*f)(void *),
 	list_ptr = begin_list;
 	while (list_ptr)
 	{
-		if ((*cmp)(list_ptr->data, data_ref) == 0)
+		if (((int (*)(void *, void *))cmp)(list_ptr->data, data_ref) == 0)
 			(*f)(list_ptr->data);
 		list_ptr = list_ptr->next;
 	}
