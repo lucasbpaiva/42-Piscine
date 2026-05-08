7d3f6c3a794ad5f90b33e19b5978afcd5e336a0e
Lucas Paiva <lpaiva234@gmail.com>
2026-05-08 12:07:00 -0300

    Add helper functions

--- C12/ex06/ft_list.h
+++ C12/ex06/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/05 16:01:49 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/08 11:57:49 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,15 @@ typedef struct s_list
 	void			*data;
 }	t_list;
 
-t_list	*ft_create_elem(void *data);
 int		ft_list_size(t_list *begin_list);
+char	*ft_strdup(char *src);
+void	ft_putstr(char *str);
+void	ft_print_list(t_list *node);
+void	ft_list_push_back(t_list **begin_list, void *data);
 void	ft_list_push_front(t_list **begin_list, void *data);
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *));
+t_list	*ft_create_elem(void *data);
+t_list	*ft_list_last(t_list *begin_list);
+t_list	*ft_list_push_strs(int size, char **strs);
 
 #endif
