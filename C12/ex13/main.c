20f00fba2fc0525685cd37f3081601f4442b24b1
Lucas Paiva <lpaiva234@gmail.com>
2026-05-18 17:56:28 -0300

    Fix memory leak

--- C12/ex13/main.c
+++ C12/ex13/main.c
@@ -6,22 +6,26 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 22:30:02 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/08 23:03:46 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/18 17:55:40 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 #include <stdlib.h>
 
+void	free_nothing(void *data)
+{
+	(void)data;
+	return ;
+}
+
 int	main(void)
 {
 	t_list	*list_a;
 	t_list	*list_b;
-	t_list	*list_c;
 
 	list_a = NULL;
 	list_b = NULL;
-	list_c = NULL;
 	ft_list_push_front(&list_a, "A2");
 	ft_list_push_front(&list_a, "A1");
 	ft_list_push_front(&list_b, "B2");
@@ -31,13 +35,10 @@ int	main(void)
 	ft_print_list(list_a);
 	ft_putstr("List B: ");
 	ft_print_list(list_b);
-	ft_putstr("List C: ");
-	ft_print_list(list_c);
 	ft_list_merge(&list_a, list_b);
-	ft_putstr("\n--- After Merge ---\nList A merged with list B: ");
+	ft_putstr("\n--- After Merge ---\n");
+	ft_putstr("List A merged with list B: ");
 	ft_print_list(list_a);
-	ft_list_merge(&list_c, list_b);
-	ft_putstr("Empty list C merged with list B: ");
-	ft_print_list(list_c);
+	ft_list_clear(list_a, free_nothing);
 	return (0);
 }
