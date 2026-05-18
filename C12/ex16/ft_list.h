3f19630035ac57c4fcd2c19b48c3fc6b3608edf0
Lucas Paiva <lpaiva234@gmail.com>
2026-05-18 18:19:52 -0300

    Fix memory leak

--- C12/ex16/ft_list.h
+++ C12/ex16/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/08 10:46:39 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/18 18:16:40 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,27 @@ typedef struct s_list
 
 int		ft_strcmp(char *s1, char *s2);
 int		ft_list_size(t_list *begin_list);
+char	*ft_strdup(char *src);
 void	ft_putstr(char *str);
 void	ft_print_list(t_list *node);
 void	ft_list_reverse(t_list **begin_list);
+void	ft_list_reverse_fun(t_list *begin_list);
 void	ft_list_sort(t_list **begin_list, int (*cmp)());
 void	simpler_sort(t_list **begin_list, int (*cmp)());
 void	ft_list_push_back(t_list **begin_list, void *data);
 void	ft_list_push_front(t_list **begin_list, void *data);
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *));
+void	ft_list_merge(t_list **begin_list1, t_list *begin_list2);
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *));
 void	ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)());
+void	ft_list_foreach_if(t_list *begin_list, void (*f)(void *),
+			void *data_ref, int (*cmp)());
+void	ft_list_remove_if(t_list **begin_list, void *data_ref,
+			int (*cmp)(), void (*free_fct)(void *));
 t_list	*ft_create_elem(void *data);
+t_list	*ft_list_last(t_list *begin_list);
 t_list	*ft_list_push_strs(int size, char **strs);
+t_list	*ft_list_at(t_list *begin_list, int nbr);
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)());
 
 #endif
--- C12/ex16/main.c
+++ C12/ex16/main.c
@@ -6,18 +6,24 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 18:26:35 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/08 10:47:44 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/18 18:18:15 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+
+void	free_nothing(void *data)
+{
+	(void)data;
+	return ;
+}
 
 int	main(int argc, char *argv[])
 {
 	t_list	*my_list;
 	char	*word_to_insert;
 
-	word_to_insert = "42";
+	word_to_insert = "FORTY-TWO";
 	if (argc < 2)
 		return (0);
 	my_list = ft_list_push_strs(argc - 1, &argv[1]);
@@ -27,5 +33,6 @@ int	main(int argc, char *argv[])
 	ft_sorted_list_insert(&my_list, word_to_insert, ft_strcmp);
 	ft_putstr("With inserted word\n");
 	ft_print_list(my_list);
+	ft_list_clear(my_list, free_nothing);
 	return (0);
 }
