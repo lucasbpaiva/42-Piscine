6d7b6f9b7c52657eb959b133bee8dbef689cc5db
Lucas Paiva <lpaiva234@gmail.com>
2026-05-18 10:44:20 -0300

    Fix memory leak

--- C12/ex03/ft_list.h
+++ C12/ex03/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:20:21 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/08 11:17:58 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:40:57 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_list_size(t_list *begin_list);
 void	ft_putstr(char *str);
 void	ft_print_list(t_list *node);
 void	ft_list_push_front(t_list **begin_list, void *data);
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *));
 t_list	*ft_create_elem(void *data);
 t_list	*ft_list_last(t_list *begin_list);
 t_list	*ft_list_push_strs(int size, char **strs);
--- C12/ex03/ft_list_clear.c
+++ C12/ex03/ft_list_clear.c
@@ -1,36 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_list_clear.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/08 11:14:11 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/18 10:42:15 by lbalderr         ###   ########.fr       */
+/*   Created: 2026/05/05 23:49:45 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/06 00:11:59 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "ft_list.h"
 
-void	free_nothing(void *data)
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
 {
-	(void)data;
-	return ;
-}
+	t_list	*head;
+	t_list	*next;
 
-int	main(int argc, char *argv[])
-{
-	t_list	*my_list;
-	t_list	*last_elem;
-
-	if (argc < 2)
-		return (0);
-	my_list = ft_list_push_strs(argc - 1, &argv[1]);
-	ft_print_list(my_list);
-	last_elem = ft_list_last(my_list);
-	ft_putstr("Last element: ");
-	ft_putstr((char *)last_elem->data);
-	ft_putstr("\n");
-	ft_list_clear(my_list, free_nothing);
-	return (0);
+	if (!free_fct)
+		return ;
+	head = begin_list;
+	while (head)
+	{
+		next = head->next;
+		free_fct(head->data);
+		free(head);
+		head = next;
+	}
 }
