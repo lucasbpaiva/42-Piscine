29a2a7a6074f3de3b615743545a23b092dbf90e9
Lucas Paiva <lpaiva234@gmail.com>
2026-05-18 18:14:59 -0300

    Fix memory leak

--- C12/ex15/ft_list_clear.c
+++ C12/ex15/ft_list_clear.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_list_clear.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/06 11:26:17 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/18 18:11:32 by lbalderr         ###   ########.fr       */
+/*   Created: 2026/05/05 23:49:45 by lbalderr          #+#    #+#             */
+/*   Updated: 2026/05/06 00:11:59 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "ft_list.h"
 
-void	free_nothing(void *data)
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
 {
-	(void)data;
-	return ;
-}
+	t_list	*head;
+	t_list	*next;
 
-int	main(int argc, char *argv[])
-{
-	t_list	*my_list;
-
-	if (argc < 2)
-		return (0);
-	my_list = ft_list_push_strs(argc - 1, &argv[1]);
-	ft_putstr("Original list: ");
-	ft_print_list(my_list);
-	ft_list_reverse_fun(my_list);
-	ft_putstr("Reversed list: ");
-	ft_print_list(my_list);
-	ft_list_clear(my_list, free_nothing);
-	return (0);
+	if (!free_fct)
+		return ;
+	head = begin_list;
+	while (head)
+	{
+		next = head->next;
+		free_fct(head->data);
+		free(head);
+		head = next;
+	}
 }
