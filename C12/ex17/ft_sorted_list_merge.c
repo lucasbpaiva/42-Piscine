e4e04bf8a57c9a70b2f104570801969a8ed5d3f7
lbalderr <lbalderr@student.42sp.org.br>
2026-05-19 15:57:52 -0300

    Refactor ft_sorted_list_merge

--- C12/ex17/ft_sorted_list_merge.c
+++ C12/ex17/ft_sorted_list_merge.c
@@ -6,7 +6,7 @@
 /*   By: lbalderr <lbalderr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 11:04:17 by lbalderr          #+#    #+#             */
-/*   Updated: 2026/05/19 12:33:58 by lbalderr         ###   ########.fr       */
+/*   Updated: 2026/05/19 14:26:07 by lbalderr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	ft_sorted_list_merge(t_list **begin_list1, t_list *begin_list2,
 	int (*cmp)())
 {
-	t_list	*cur_1;
-	t_list	*nxt_2;
-	int		(*f)(void *, void *);
+	t_list	**curr;
+	t_list	*next;
+	int		(*f_cmp)(void *, void *);
 
 	if (!begin_list1 || !begin_list2 || !cmp)
 		return ;
@@ -26,26 +26,17 @@ void	ft_sorted_list_merge(t_list **begin_list1, t_list *begin_list2,
 		*begin_list1 = begin_list2;
 		return ;
 	}
-	f = (int (*)(void *, void *))cmp;
-	while (begin_list2 && f(begin_list2->data, (*begin_list1)->data) <= 0)
+	f_cmp = (int (*)(void *, void *))cmp;
+	curr = begin_list1;
+	while (begin_list2)
 	{
-		nxt_2 = begin_list2->next;
-		begin_list2->next = *begin_list1;
-		*begin_list1 = begin_list2;
-		begin_list2 = nxt_2;
-	}
-	cur_1 = *begin_list1;
-	while (cur_1->next && begin_list2)
-	{
-		if (f(begin_list2->data, cur_1->next->data) <= 0)
+		if (!(*curr) || f_cmp(begin_list2->data, (*curr)->data) <= 0)
 		{
-			nxt_2 = begin_list2->next;
-			begin_list2->next = cur_1->next;
-			cur_1->next = begin_list2;
-			begin_list2 = nxt_2;
+			next = begin_list2->next;
+			begin_list2->next = *curr;
+			*curr = begin_list2;
+			begin_list2 = next;
 		}
-		cur_1 = cur_1->next;
+		curr = &((*curr)->next);
 	}
-	if (begin_list2)
-		cur_1->next = begin_list2;
 }
